c98da73aea595655cdec7fcc8cb0e9896f81235c
Zumite27 <zakowska.zuzanna@gmail.com>
2024-10-23 11:16:49 +0200

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,27 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    ResourceManager(){
+        Resource newRes{};
+        myResource = newRes;
+    }
+
+    ResourceManager(const ResourceManager& other){
+        myResource = other.myResource;
+    }
+
+    ~ResourceManager(){}
+
+    ResourceManager& operator=(const ResourceManager& other){
+        if (this == &other){return *this;}
+        myResource = other.myResource;
+        return *this;
+    }
+
+    double get(){ return myResource.get();}
+
+    Resource myResource;
+
+private:
 };
