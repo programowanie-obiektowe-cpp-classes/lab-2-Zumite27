d7b842886f47c651965489fd53f6f5a7bd761389
Zuzanna Żakowska <zakowska.zuzanna@gmail.com>
2024-12-03 14:31:21 +0100

    Uzupelnione rozwiazanie zejsciowki nr 2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,31 @@
 #pragma once
 
+#include <memory>
 #include "Resource.hpp"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    ResourceManager() : resource_{std::make_unique<Resource>()} {}
+
+    ResourceManager(const ResourceManager& other): resource_{std::make_unique<Resource>(*other.resource_)} {}
+
+    ResourceManager& operator=(const ResourceManager& other) {
+        if (this == &other) return *this;
+        resource_ = std::make_unique<Resource>(*other.resource_);
+        return *this;
+    }
+
+    ResourceManager(ResourceManager&& other) noexcept = default;
+
+    ResourceManager& operator=(ResourceManager&& other) noexcept = default;
+
+    ~ResourceManager() = default;
+
+    double get() const {
+        return resource_->get();
+    }
+
+private:
+    std::unique_ptr<Resource> resource_;
 };
